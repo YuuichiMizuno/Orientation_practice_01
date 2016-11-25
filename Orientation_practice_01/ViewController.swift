df916975d26418bf134b0153653d6ec6a942785f
yuichi.watanabe <yuichi.watanabe@booklive.co.jp>
2016-11-25 12:10:38 +0900

    Initial Commit

---
+++ Orientation_practice_01/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Orientation_practice_01
+//
+//  Created by yuichi.watanabe on 2016/11/25.
+//  Copyright © 2016年 yuuichi.watanabe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
